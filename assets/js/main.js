c11095bd7d804550d6ffce81826134328a84fe95
yvanlebras <yvanlebras@users.noreply.github.com>
2022-02-01 01:19:20 +0000

    deploy: 86ba6f30c3c8e76d2a8f07c2a333a13a48cabfc5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // Handle foldable challenges and solutions (on click and at start).
-$(".solution,.details").click(function(event) {
-    $(">*:not(h3)", this).toggle(400);
-    $(">h3>span.fold-unfold", this).toggleClass("fa-plus-square fa-minus-square");
+$(".solution>h3,.details>h3,.tip>h3").click(function(event) {
+    $(">*:not(h3)", $(this).parent()).toggle(400);
+    $(">span.fold-unfold", this).toggleClass("fa-plus-square fa-minus-square");
 });
 
-$(".solution,.details").each(function() {
+$(".solution,.details,.tip").each(function() {
     $(">*:not(h3)", this).toggle();
     var h3 = $("h3:first", this);
-    h3.append("<span class='fold-unfold fa fa-plus-square'></span>");
+    h3.append("<span role='button' class='fold-unfold fa fa-plus-square'></span>");
 });
 
 (function (window, document) {
@@ -23,13 +23,21 @@ $(".solution,.details").each(function() {
         // If you pass `?with-answers` to an URL, it will automatically open
         // the `<details>` blocks (i.e. the Q&A sections most of the time).
         if (window.location.search.match(/\?with-answers/gi)) {
-            // Loop over all the `<details>` tags and open them.
-            var details = document.querySelectorAll('details');
-            Array.prototype.forEach.call(details, function (detail) {
-                detail.querySelector('summary').textContent = 'Answers';
-                detail.setAttribute('open', true);
-            });
+            // Same as above selector
+            $(".solution>h3,.details>h3,.tip>h3").click();
+
         }
     });
 
 })(window, document);
+
+
+<!--  For admin training -->
+$("section.tutorial.topic-admin div.language-diff pre code .gi,section.tutorial.topic-admin div.language-diff pre code .gd").each((x, e) => {
+  var fixed = $(e).text().split('\n').map(q => { return q.slice(1) }).join('\n');
+  $(e).text(fixed);
+})
+$("section.tutorial.topic-data-science div.language-diff pre code .gi,section.tutorial.topic-data-science div.language-diff pre code .gd").each((x, e) => {
+  var fixed = $(e).text().split('\n').map(q => { return q.slice(1) }).join('\n');
+  $(e).text(fixed);
+})
